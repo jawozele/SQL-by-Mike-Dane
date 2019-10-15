9665775322e4aa2d7b2675018f7b3fbefadb0a54
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-15 22:04:44 +0100

    Update alters.sql

--- alters.sql
+++ alters.sql
@@ -1,18 +1,25 @@
 
-
+-- primary key for the employees table
 ALTER TABLE employees
 ADD CONSTRAINT pk_emp_id
 PRIMARY KEY (emp_id);
 
+-- branches table primary key
 ALTER TABLE branches
 ADD CONSTRAINT pk_branch_id
 PRIMARY KEY (branch_id);
 
+-- clients table primary key
 ALTER TABLE clients
 ADD CONSTRAINT pk_client_id
 PRIMARY KEY (client_id);
 
+-- works_with table primary key
 ALTER TABLE works_with
 ADD CONSTRAINT pk_emp_id
 PRIMARY KEY (emp_id);
 
+-- managers table primary key
+ALTER TABLE managers
+ADD CONSTRAINT mgr_id
+PRIMARY KEY (mgr_id);
