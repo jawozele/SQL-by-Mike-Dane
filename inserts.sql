f729043b725ac7f6eda79345e6e15be8435c657d
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-15 22:05:43 +0100

    Add files via upload

---
+++ inserts.sql
@@ -0,0 +1,180 @@
+
+/*
+branch_suppliers table
+*/
+
+
+INSERT INTO branch_suppliers
+VALUES (1, 'Ryman Stationery Southampon', 'Office branch_suppliers');
+
+
+INSERT INTO branch_suppliers
+VALUES (2, 'A&A Business Supplies', 'Art Supplies');
+
+
+INSERT INTO branch_suppliers
+VALUES (3, 'Perrys Art Suppliers Ltd', 'Books, Pens, Writing Pads'  );
+
+
+INSERT INTO branch_suppliers
+VALUES (4, 'Southampton Business Supplies', 'Business Supplies');
+
+
+INSERT INTO branch_suppliers
+VALUES (5, 'Absolute Business Change Ltd', 'Office Supplies');
+
+
+INSERT INTO branch_suppliers
+VALUES (6, 'Inter County Office Supplies Ltd', 'Business Supplies');
+
+
+INSERT INTO branch_suppliers
+VALUES (7, 'ISL Office Solutions', 'Business Supplies');
+
+
+/*
+employees table
+*/
+
+INSERT INTO employees
+VALUE (100, 'Nancy', 'Davolio', '1952-02-19', 'M', 94000, 200, 1);
+
+
+INSERT INTO employees
+VALUE (200, 'Andrew', 'Fuller', '1968-12-08', 'F', 96000, 200, 2);
+
+INSERT INTO employees
+VALUE (300, 'Michael', 'Suyama', '1963-02-02', 'M', 59000, 300, 3);
+
+
+INSERT INTO employees
+VALUE (400, 'Robert', 'King', '1960-05-29', 'M', 101000, 400, 4);
+
+
+INSERT INTO employees
+VALUE (500, 'Laura', 'Callahan', '1958-01-09', 'F', 101200, 500, 5);
+
+
+INSERT INTO employees
+VALUE (600, 'Anne', 'Dodsworth', '1969-07-02', 'F', 89000, 600, 6);
+
+
+INSERT INTO employees
+VALUE (700, 'Steven', 'Buchanan', '1955-03-04', 'M', 300000, 700, 7);
+
+
+
+INSERT INTO employees
+VALUE (800, 'Kala', 'Wilman', '1987-12-02', 'M', 1011000, 800, 8);
+
+
+
+INSERT INTO employees
+VALUE (900, 'Wolski', 'Zbyszek', '1979-01-10', 'M', 34000, 900, 9);
+
+
+/*
+branches table
+*/
+
+
+INSERT INTO branches
+VALUE (1, 'Southampton', 200, '2007-12-12');
+
+
+
+INSERT INTO branches
+VALUE (2, 'London', 300, '2009-01-13');
+
+
+
+INSERT INTO branches
+VALUE (3, 'Birmingham', 300, '2009-03-05');
+
+
+
+INSERT INTO branches
+VALUE (4, 'Liverpool', 400, '2015-01-03');
+
+
+/*
+clients table
+*/
+
+
+INSERT INTO clients
+VALUE (1, 'John Lewis', 1);
+
+INSERT INTO clients
+VALUE (2, 'Marks & Spencer', 2);
+
+INSERT INTO clients
+VALUE (3, 'H & M', 3);
+
+
+/*
+managers table
+*/
+
+
+INSERT INTO managers 
+VALUE ('Southampton', 100, '2007-12-12');
+
+
+INSERT INTO managers 
+VALUE ('Southampton', 100, '2007-12-12');
+
+INSERT INTO managers 
+VALUE ('London', 200, '2009-01-13');
+
+INSERT INTO managers 
+VALUE ('Birmingham', 300, '2009-03-05');
+
+
+INSERT INTO managers 
+VALUE ('Liverpool', 400, '2015-01-03');
+
+
+/*
+works_with table
+*/
+
+INSERT INTO works_with
+VALUE (100, 1, 500000);
+
+
+INSERT INTO works_with
+VALUE (200, 2, 3450340);
+
+INSERT INTO works_with
+VALUE (300, 3, 320000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
